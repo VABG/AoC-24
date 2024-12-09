e143fa5e3559be7708b4c73ec276ee4751ed89dd
VABG <VABGProductions@hotmail.com>
2024-12-09 11:36:09 +0100

    Clipboard Support

--- AdventOfCode_24/Model/Logging/LogMessage.cs
+++ AdventOfCode_24/Model/Logging/LogMessage.cs
@@ -1,4 +1,7 @@
 using Avalonia.Media;
+using System.Windows;
+using Avalonia;
+using Avalonia.Controls;
 
 namespace AdventOfCode_24.Model.Logging;
 
@@ -6,4 +9,12 @@ public class LogMessage(string message, Color color)
 {
     public string Message { get; } = message;
     public Color Color { get; } = color;
+
+    public void CopyToClipboard(object msg)
+    {
+        if (msg is not Visual visual)
+            return;
+        var topLevel = TopLevel.GetTopLevel(visual);
+        topLevel?.Clipboard?.SetTextAsync(message);
+    }
 }
